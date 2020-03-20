9f6e785826309ef0e6a116e84cdc43a1f1877826
Matthias Kutz <kutz.matthias@gmail.com>
2020-03-20 15:56:23 -0700

    Backend initial hinzugefügt

---
+++ einkaufen-digital-backend/src/main/java/org/wirvsvirushackathon/einkaufendigitalbackend/EinkaufenDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package org.wirvsvirushackathon.einkaufendigitalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufenDigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinkaufenDigitalBackendApplication.class, args);
+	}
+
+}
