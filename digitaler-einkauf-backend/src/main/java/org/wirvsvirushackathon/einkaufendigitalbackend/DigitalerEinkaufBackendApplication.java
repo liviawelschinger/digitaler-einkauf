eb91cf8ae4e49f54389aa69f3dedc167406d6427
Matthias Kutz <kutz.matthias@gmail.com>
2020-03-21 02:56:18 -0700

    Swagger-UI hinzugefügt

--- digitaler-einkauf-backend/src/main/java/org/wirvsvirushackathon/einkaufendigitalbackend/DigitalerEinkaufBackendApplication.java
+++ digitaler-einkauf-backend/src/main/java/org/wirvsvirushackathon/einkaufendigitalbackend/DigitalerEinkaufBackendApplication.java
@@ -2,8 +2,10 @@ package org.wirvsvirushackathon.einkaufendigitalbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DigitalerEinkaufBackendApplication {
 
 	public static void main(String[] args) {
