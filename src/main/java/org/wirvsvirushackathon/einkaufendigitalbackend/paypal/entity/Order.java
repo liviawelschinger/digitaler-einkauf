20026ecb6d49e5cb49abfa4bcdb18c43460bc147
malex5335 <maximilian.rich@online.de>
2020-03-22 15:22:09 +0100

    Korrektur Paypal Order-Entity

--- src/main/java/org/wirvsvirushackathon/einkaufendigitalbackend/paypal/entity/Order.java
+++ src/main/java/org/wirvsvirushackathon/einkaufendigitalbackend/paypal/entity/Order.java
@@ -11,10 +11,50 @@ import lombok.ToString;
 @ToString
 public class Order {
 
-    private double price;
-    private String currency;
-    private String method;
-    private String intent;
-    private String description;
+	private double price;
+	private String currency;
+	private String method;
+	private String intent;
+	private String description;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public String getIntent() {
+		return intent;
+	}
+
+	public void setIntent(String intent) {
+		this.intent = intent;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
